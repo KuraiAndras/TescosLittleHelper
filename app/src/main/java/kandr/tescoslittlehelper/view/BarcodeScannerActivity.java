3188a0222c311821aa01e324e1c9e53d64f5d665
András <k.andras9642@gmail.com>
2018-12-02 11:52:18 +0100

    on insert vibrate and finish

--- app/src/main/java/kandr/tescoslittlehelper/view/BarcodeScannerActivity.java
+++ app/src/main/java/kandr/tescoslittlehelper/view/BarcodeScannerActivity.java
@@ -1,6 +1,9 @@
 package kandr.tescoslittlehelper.view;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.util.SparseArray;
 import android.view.View;
@@ -31,6 +34,7 @@ public class BarcodeScannerActivity extends AppCompatActivity implements Barcode
             @Override
             public void onClick(View view) {
                 DbManager.insertIntoDatabase(getApplicationContext(), DbManager.getMockedProductData());
+                vibrateAndFinish();
             }
         });
     }
@@ -39,10 +43,19 @@ public class BarcodeScannerActivity extends AppCompatActivity implements Barcode
         btnAddMockedProduct = findViewById(R.id.btnAddMockedProduct);
     }
 
+    private void vibrateAndFinish(){
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (vibrator != null) {
+            vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        }
+        finish();
+    }
+
     @Override
     public void onScanned(Barcode barcode) {
         ProductData productData = NetworkManager.getInstance().getProduct(barcode.displayValue);
         DbManager.insertIntoDatabase(getApplicationContext(), productData);
+        vibrateAndFinish();
     }
 
     @Override
